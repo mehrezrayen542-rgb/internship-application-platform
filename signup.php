aa8aa208ba5ac98bd90074c3d6ea2e7ce243c7fb
Skanoul <swannkentzel5@gmail.com>
2026-01-17 15:24:47 +0100

    add more in php signup

--- signup.php
+++ signup.php
@@ -10,15 +10,23 @@ try {
     die('Impossible de se connecter à la base de données, veuillez vérifier les données de connexion !: ' . $e->getMessage());
 }
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $account_type = isset($_POST["account_type"]) ? htmlspecialchars($_POST["account_type"]) : "";
-    $email = isset($_POST["email"]) ? htmlspecialchars($_POST["email"]) : "";
-    $first_name = isset($_POST["fst"]) ? htmlspecialchars($_POST["fst"]) : "";
-    $last_name = isset($_POST["lst"]) ? htmlspecialchars($_POST["lst"]) : "";
-    if ($account_type === "student") {
+    $account_type = $_POST["account_type"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
+    if ($account_type == "student") {
+        $first_name = $_POST["fst"];
+        $last_name = $_POST["lst"];
+        $university = $_POST["university"];
+        $lvl = $_POST["lvl"];
+        $study = $_POST["study"];
+
+    } else {
+        $campany_name = $_POST["campany_name"];
+        $campany_website = $_POST["campany_website"];
+        $company_size = $_POST["company_size"];
     }
-
+    #verifier que lemail existe deja
 
 }
-
 ?>
