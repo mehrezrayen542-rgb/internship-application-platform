29264c471d830c7256679a15113eef135574c8f8
mehrezrayen542-rgb <mehrezrayen542@gmail.com>
2026-01-17 15:50:03 +0100

    ajout de signin.php

--- signin.php
+++ signin.php
@@ -1,4 +1,14 @@
 <?php
 require ("connection.php");
-$name=$_POST
+$name=$_POST["name"];
+$mdp=$_POST["mdp"];
+$req="SELECT * from users where email='$name' and password='$mdp'";
+$res= mysqli_query($c,$req);
+if (mysqli_num_rows($res)==1){
+    header("Location: index.php");
+    exit;
+}
+else{
+    die ('le mot de passe est incorrect'); 
+}
 ?>
